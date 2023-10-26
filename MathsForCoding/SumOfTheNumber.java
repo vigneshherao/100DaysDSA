47167fdab2efef4cb444140f1708eb2f5c737dfd
vignesh <vigneshfornavy@gmail.com>
2023-10-26 20:38:54 +0530

    sumofallnumbers

---
+++ MathsForCoding/SumOfTheNumber.java
@@ -0,0 +1,14 @@
+package MathsForCoding;
+
+public class SumOfTheNumber {
+    public static void main(String[] args) {
+        int num = 1234;
+        int sum = 0;
+        while(num!=0){
+            int lastDigit = num % 10;
+            sum = sum + lastDigit;
+            num = num/10;
+        }
+        System.out.println(sum);
+    }
+}
