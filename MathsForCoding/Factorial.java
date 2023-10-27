00df2f376f5134a1bd57b25c76492c14d705a4ef
vignesh <vigneshfornavy@gmail.com>
2023-10-27 18:51:17 +0530

    factorial Of a Number

---
+++ MathsForCoding/Factorial.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Factorial{
+    public static void main(String[] args) {
+        //will take number from the user
+        Scanner obj = new Scanner(System.in);
+        int userInput = obj.nextInt();
+        int product = 1;
+        for(int i = 1; i<=userInput;i++){
+            product = product * i;
+        }
+        System.out.println(product);
+    }
+}
---
+++ MathsForCoding/PrimeorNot.java
@@ -0,0 +1,12 @@
+public class PrimeorNot {
+    public static void main(String[] args) {
+        int a = 10;
+        for(int i = 2;i<Math.sqrt(a);i++){
+            if(a%2==0){
+                System.out.println("not a prime");
+                break;
+            }
+        }
+        System.out.println("prime number");
+    }
+}
